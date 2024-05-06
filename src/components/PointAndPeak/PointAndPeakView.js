d4be8b379ff2288f73eebcd43d8cef4e91270eff
Adrian S <adrian.salinas.1986@gmail.com>
2024-05-06 11:34:33 -0600

    removed unused state for demo

--- src/components/PointAndPeak/PointAndPeakView.js
+++ src/components/PointAndPeak/PointAndPeakView.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 import PointAndPeakCards from "./PointAndPeakCards";
 
--- src/components/ProvisioningFailure/ProvisioningFailureView.js
+++ src/components/ProvisioningFailure/ProvisioningFailureView.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 import ProvisioningFailureCards from "./ProvisioningFailureCards";
 
